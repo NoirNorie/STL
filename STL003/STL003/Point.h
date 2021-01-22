782c9132e9877d3109b0279210c1a28419d3e533
LEE MYEONG JIN <lmjking1@gmail.com>
2021-01-22 19:21:19 +0900

    연산자 오버로딩
    STL003
    
    연산자 오버로딩
    이항, 단항, 논리

---
+++ STL003/STL003/Point.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "stdafx.h"
+
+class Point
+{
+	int x;
+	int y;
+public:
+	Point(int _x = 0, int _y = 0) :x(_x), y(_y) {}
+	
+	// 이항 연산자의 오버로딩
+	const Point operator+(const Point& arg) const;
+	const Point operator-(const Point& arg) const;
+
+	// 논리 이항 연산자의 오버로딩
+	const bool operator==(const Point& arg) const;
+	const bool operator!=(const Point& arg) const;
+
+	// 단항 연산자 ++의 오버로딩
+	const Point& operator++(); // 전위
+	const Point& operator++(int); // 후위
+
+	// 단항 연산자 --의 오버로딩
+	const Point& operator--(); // 전위
+	const Point& operator--(int); // 후위
+	
+	// Getter와 Setter
+	int GetX() const;
+	int GetY() const;
+	void SetX(int _x);
+	void SetY(int _y);
+
+	// 출력함수
+	void Print() const;
+};
---
+++ STL003/STL003/STL003.cpp
@@ -0,0 +1,72 @@
+﻿// STL003.cpp : 연산자 오버로딩
+//
+
+#include "stdafx.h" // 미리 컴파일된 헤더
+#include "Point.h" // 점의 정의
+
+int main()
+{
+    Point p1(2, 3), p2(4, 5);
+    Point result1, result2; // 결과 값을 담을 공간
+    Point result3;
+
+    // 이항 연산자 + 사용
+    cout << "이항연산자 +" << endl;
+    result3 = p2 + p1;
+    result3.Print();
+
+    // 이항 연산자 - 사용
+    cout << "이항연산자 -" << endl;
+    result3 = p2 - p1;
+    result3.Print();
+
+    cout << "단항연산자 ++" << endl;
+    cout << "연산 전 p1 : " << p1.GetX() << ", " << p1.GetY() << endl;
+
+    result1 = ++p1;
+    // result1 = p1.operator++() 과 동일함
+    result1.Print();
+
+    cout << "단항연산자 --" << endl;
+    cout << "연산 전 p1 : " << p1.GetX() << ", " << p1.GetY() << endl;
+
+    result1 = --p1;
+    // result1 = p1.operator++() 과 동일함
+    result1.Print();
+
+    cout << "단항연산자 ++(후위)" << endl;
+    cout << "연산 전 p2 : " << p2.GetX() << ", " << p2.GetY() << endl;
+
+    result2 = p2++;
+    // result2 = p2.oprator++(0) 과 동일함
+    // 더미값인 0을 입력해야 후위 연산으로 동작함
+    result2.Print();
+
+    cout << "연산 후 p2 : " << p2.GetX() << ", " << p2.GetY() << endl;
+
+    cout << "단항연산자 --(후위)" << endl;
+    cout << "연산 전 p2 : " << p2.GetX() << ", " << p2.GetY() << endl;
+
+    result2 = p2--;
+    // result2 = p2.oprator++(0) 과 동일함
+    // 더미값인 0을 입력해야 후위 연산으로 동작함
+    result2.Print();
+
+    cout << "연산 후 p2 : " << p2.GetX() << ", " << p2.GetY() << endl;
+
+
+    // 논리 이항연산자 오버로딩
+
+    Point a1(2, 3), a2(3, 4), a3(2, 3);
+
+    if (a1 == a2) cout << "같음" << endl;
+    else cout << "다름" << endl;
+
+    if (a1 == a3) cout << "같음" << endl;
+    else cout << "다름" << endl;
+
+    if (a1 != a2) cout << "다름" << endl;
+    else cout << "같음" << endl;
+
+    return 0;
+}
