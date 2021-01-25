d4e1b392ef978f9ca3a657afaec204b1847c7ca8
LEE MYEONG JIN <lmjking1@gmail.com>
2021-01-25 23:28:26 +0900

    타입 변환 연산자 오버로딩
    STL011
    
    타입 변환 연산자 오버로딩을 통한 타입 변환

---
+++ STL011/STL011/STL011.cpp
@@ -0,0 +1,18 @@
+﻿// STL011.cpp : 타입 변환 연산자 오버로딩
+// 타입 변환 연산자 오버로딩을 이용한 타입변환
+
+#include "stdafx.h"
+#include "Point.h"
+
+int main()
+{
+    int n = 10;
+
+    Point pt(2, 3);
+    n = pt;
+    // pt.operator int()를 암시적으로 호출한다.
+
+    cout << n << endl;
+
+    return 0;
+}
